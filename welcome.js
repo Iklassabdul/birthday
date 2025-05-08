4c0484f834f6c5ba368269826ee1dc9906474de7
iklass abdul <iklassabdul@gmail.com>
2025-05-08 13:10:49 -0700

    Hihaj

--- welcome.js
+++ welcome.js
@@ -9,19 +9,19 @@ document.addEventListener("DOMContentLoaded", function () {
     "Oluwakemi,<br>Some people write letters. I build webpages.<br>And this one? It’s all for you.",
     "Hey beautiful,<br>I didn’t just build a page... I planted a surprise that blooms on your birthday.",
     "You may not see the effort...<br>But every second behind this was for one thing.<br>Making your birthday feel a little more special, Oluwakemi.",
-    "Good afternoon, Oluwakemi.<br>This page exists for one reason:<br>You. And the joy of seeing you smile.",
+    "Oluwakemi.<br>This page exists for one reason:<br>You. And the joy of seeing you smile.",
     "Hi Oluwakemi,<br>They say love is shown in little things.<br>So here’s a page, counting down to a day that means everything to me.",
     "Oluwakemi,<br>I coded a surprise into the future.<br>Keep checking... it’s going to be sweet.",
     "Some gifts come wrapped in ribbons,<br>Others, in intention.<br>This one is wrapped in code, crafted for you, Oluwakemi.",
     "Hi Oluwakemi,<br>A little something to remind you how special you are...<br>Let the countdown to your day begin.",
     "Oluwakemi,<br>This isn’t just a website... it’s a journey.<br>And it all leads to your birthday.",
-    "Good afternoon, Oluwakemi!<br>I may not be a magician...<br>But I made this just for you. Let the countdown begin.",
+    "Oluwakemi!<br>I may not be a magician...<br>But I made this just for you. Let the countdown begin.",
     "Dear Oluwakemi,<br>Every moment, every second... brings us closer to your light.<br>This page was built with love, care, and a little hope to make you smile.",
-    "Hello Oluwakemi,<br>This is for you.<br>A quiet countdown to something special.",
+    "Oluwakemi,<br>This is for you.<br>A quiet countdown to something special.",
     "Oluwakemi,<br>This isn’t just a countdown...<br>It’s my way of showing how much I care.<br>Built with love just for you.",
     "To my favorite person,<br>Every second brings us closer to your day.<br>This page was crafted with all my heart.",
     "Hey Oluwakemi,<br>Something special is waiting for you here.<br>Let’s count down to it together.",
-    "Good afternoon, Oluwakemi.<br>I didn’t wrap this in paper, I built it in code.<br>A gift made with love... and a few lines of JavaScript."
+    "Oluwakemi.<br>I didn’t wrap this in paper, I built it in code.<br>A gift made with love... and a few lines of JavaScript."
   ];
 
   const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
